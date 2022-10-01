d35b2898c0a869f5d8e6558a27d3dbb3599b4446
Sara Hampson <shampson518@gmail.com>
2022-10-01 14:09:50 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { ConnectableObservable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'hello-angular';
+  width: number = 250;
+  height: number = 200;
+  isDisabled: boolean = true;
+  title: string = 'header';
+  isShow = true;
+  isShowContent = true;
+  secondTitle = 'header';
+  myColor: string = 'green';
+  changeColor: string = 'green';
+  foods: string[] = ['food1', 'food2', 'food3'];
+
+  onClick() {
+    console.log('It works!');
+  }
+
+  onUpdateName(event: Event) {
+    this.secondTitle = (<HTMLInputElement>event.target).value;
+  }
+
 }
